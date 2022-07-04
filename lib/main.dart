1235a7480fbfa3fdf6862bad11ee331d39d03488
Tapan Barman <tapanbarman@outlook.in>
2022-07-04 14:55:56 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
